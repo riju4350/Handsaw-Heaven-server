8a292cc13eccf4f3e005154b8e3a200166a0ef71
riju4350 <riju4350@gmail.com>
2022-07-24 11:08:11 +0600

    thired commit

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
+const app = express()
 const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
