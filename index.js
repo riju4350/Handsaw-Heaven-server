517cbf11ec9988161f9a02f7c7e7b1b0c1ab9c5c
riju4350 <riju4350@gmail.com>
2022-08-10 16:46:47 +0600

    admin api added

--- index.js
+++ index.js
@@ -1,40 +1,136 @@
-const express = require('express')
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://riju4350:vz1LxjZZeULxGAME@cluster0.emahksk.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.emahksk.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run() {
   try {
     await client.connect();
-    const productsCollection = client.db("handsaw-heaven").collection("products");
+    const productsCollection = client
+      .db("handsaw-heaven")
+      .collection("products");
     const specialCollection = client.db("handsaw-heaven").collection("special");
-    const reviewCollection = client.db("handsaw-heaven").collection("reivews");
-    
-    app.get('/products', async(req,res)=>{
+    const reviewCollection = client.db("handsaw-heaven").collection("reviews");
+    const ordersCollection = client.db("handsaw-heaven").collection("orders");
+    const userCollection = client.db("handsaw-heaven").collection("users");
+
+    //Get
+    app.get("/products", async (req, res) => {
       const query = {};
       const products = await productsCollection.find(query).toArray();
       res.send(products);
-    })
-    app.get('/special', async(req,res)=>{
+    });
+    //Get
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
+    //Get
+    app.get("/special", async (req, res) => {
       const query = {};
       const special = await specialCollection.find(query).toArray();
       res.send(special);
-    })
-    app.get('/reviews', async(req,res)=>{
+    });
+    //Get
+    app.get("/reviews", async (req, res) => {
       const query = {};
       const reviews = await reviewCollection.find(query).toArray();
       res.send(reviews);
-    })
+    });
+    //Post
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const reviews = await reviewCollection.insertOne(review);
+      res.send(reviews);
+    });
+    // Get
+    app.get("/orders", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+      if (decodedEmail === email) {
+        const qurey = { email: email };
+        const orders = await ordersCollection.find(qurey).toArray();
+        return res.send(orders);
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+    });
+
+    //Post
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const orders = await ordersCollection.insertOne(order);
+      res.send(orders);
+    });
+    //Put admin api
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const results = await userCollection.updateOne(filter, updateDoc);
+      res.send(results);
+    });
+    //Put user api
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const results = await userCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ results, token });
+    });
+    // Get User
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
     console.log("Connected successfully to server");
   } finally {
     // await client.close();
@@ -42,12 +138,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Handsaw Heaven!')
-})
+app.get("/", (req, res) => {
+  res.send("Handsaw Heaven!");
+});
 
 app.listen(port, () => {
-  console.log(`Heaven app listening on port ${port}`)
-})
+  console.log(`Heaven app listening on port ${port}`);
+});
