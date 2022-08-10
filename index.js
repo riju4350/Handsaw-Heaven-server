170b3c49368e5d0ff9440856f3be54c59c2de89a
riju4350 <riju4350@gmail.com>
2022-08-10 20:28:33 +0600

    portfolio api added

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ async function run() {
     const reviewCollection = client.db("handsaw-heaven").collection("reviews");
     const ordersCollection = client.db("handsaw-heaven").collection("orders");
     const userCollection = client.db("handsaw-heaven").collection("users");
+    const profileCollection = client.db("handsaw-heaven").collection("profile");
+    const portfolioCollection = client
+      .db("handsaw-heaven")
+      .collection("portfolio");
 
     //Get
     app.get("/products", async (req, res) => {
@@ -94,15 +98,30 @@ async function run() {
       const orders = await ordersCollection.insertOne(order);
       res.send(orders);
     });
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
     //Put admin api
     app.put("/user/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set: { role: "admin" },
-      };
-      const results = await userCollection.updateOne(filter, updateDoc);
-      res.send(results);
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const results = await userCollection.updateOne(filter, updateDoc);
+        res.send(results);
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
     });
     //Put user api
     app.put("/user/:email", async (req, res) => {
@@ -114,6 +133,49 @@ async function run() {
         $set: user,
       };
       const results = await userCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ results, token });
+    });
+
+    //Put profile
+    app.put("/myprofile/:email", async (req, res) => {
+      const email = req.params.email;
+      const profileInfo = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: profileInfo,
+      };
+      const results = await profileCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ results, token });
+    });
+    //Put Portfolio
+    app.put("/myPortfolio/:email", async (req, res) => {
+      const email = req.params.email;
+      const portfolioInfo = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: portfolioInfo,
+      };
+      const results = await portfolioCollection.updateOne(
         filter,
         updateDoc,
         options
